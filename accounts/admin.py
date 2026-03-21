014ee70d9bb4303ef48aa8e0e2c469c98df4bedf
Vikas <samudralavikas2005@gmail.com>
2026-03-21 11:00:40 +0530

    updated admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,13 +1,29 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
-from .models import User
+from django.contrib.auth.admin import UserAdmin
+from .models import User, LoginAuditLog
+
 
 @admin.register(User)
-class CustomUserAdmin(DjangoUserAdmin):
-    list_display = ("username", "role", "branch", "is_staff", "is_active")
-    list_filter = ("role", "branch")
-    fieldsets = (
-        (None, {"fields": ("username", "password")}),
-        ("Role & Branch", {"fields": ("role", "branch")}),
-        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
+class CustomUserAdmin(UserAdmin):
+    list_display  = ["username", "role", "branch", "is_staff", "is_active"]
+    list_filter   = ["role", "branch"]
+    fieldsets     = UserAdmin.fieldsets + (
+        ("COATS Info", {"fields": ("role", "branch")}),
     )
+
+
+@admin.register(LoginAuditLog)
+class LoginAuditLogAdmin(admin.ModelAdmin):
+    list_display  = ["username", "success", "ip_address", "timestamp", "blockchain_tx"]
+    list_filter   = ["success"]
+    search_fields = ["username", "ip_address"]
+    readonly_fields = ["username", "ip_address", "timestamp", "success", "user_agent", "blockchain_tx", "blockchain_hash", "blockchain_block", "blockchain_url"]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
--- cases/admin.py
+++ cases/admin.py
@@ -1,19 +1,45 @@
-
-# Register your models here.
 from django.contrib import admin
-from .models import Case
-
+from .models import Case, CaseLog, ChainOfCustody
 
 @admin.register(Case)
 class CaseAdmin(admin.ModelAdmin):
-    list_display = (
-        'id',
-        'crime_number',
-        'current_stage',
-        'branch',
-        'case_holding_officer',
-        'date_of_first_updation',
-    )
+    list_display  = ["crime_number", "section_of_law", "current_stage", "branch", "case_holding_officer"]
+    list_filter   = ["current_stage", "branch"]
+    search_fields = ["crime_number", "section_of_law", "complainant_name"]
 
-    list_filter = ('current_stage', 'branch')
-    search_fields = ('crime_number', 'complainant_name')
+    def has_delete_permission(self, request, obj=None):
+        return False  # Cases cannot be deleted
+
+
+@admin.register(CaseLog)
+class CaseLogAdmin(admin.ModelAdmin):
+    list_display  = ["crime_number", "field_changed", "old_value", "new_value", "updated_by", "timestamp", "blockchain_tx"]
+    list_filter   = ["field_changed", "branch"]
+    search_fields = ["crime_number", "field_changed"]
+    readonly_fields = ["case", "field_changed", "old_value", "new_value", "updated_by", "timestamp", "crime_number", "branch", "blockchain_tx", "blockchain_hash", "blockchain_block", "blockchain_url"]
+
+    def has_add_permission(self, request):
+        return False  # Cannot create logs manually
+
+    def has_change_permission(self, request, obj=None):
+        return False  # Cannot edit logs
+
+    def has_delete_permission(self, request, obj=None):
+        return False  # Cannot delete logs
+
+
+@admin.register(ChainOfCustody)
+class ChainOfCustodyAdmin(admin.ModelAdmin):
+    list_display  = ["crime_number", "action", "officer_username", "timestamp", "blockchain_tx"]
+    list_filter   = ["action", "branch"]
+    search_fields = ["crime_number", "officer_username"]
+    readonly_fields = ["case", "officer", "officer_username", "officer_role", "officer_branch", "action", "reason", "notes", "field_changed", "old_value", "new_value", "crime_number", "branch", "ip_address", "timestamp", "blockchain_tx", "blockchain_hash", "blockchain_block", "blockchain_url"]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
